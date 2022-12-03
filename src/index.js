e5efc4f016333ad270b1245c6d3c108b1ee59851
KP <kpinc425@gmail.com>
2022-12-02 18:42:02 -0800

    bundle styling for gh upload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+// import './reset.css';
+// import './style.css';
 import { changeElementOnFlexWrap, el_dropDownMenu, mobileMenu } from './mobileMenu.js';
-// import resetStyles from '../src/reset.css' assert { type: 'css' };
-// import styles from '../src/style.css' assert { type: 'css' };
-// console.log(styles);
-// document.adoptedStyleSheets = [resetStyles];
-// document.adoptedStyleSheets = [styles];
+import resetStyles from '../src/reset.css' assert { type: 'css' };
+import styles from '../src/style.css' assert { type: 'css' };
+console.log(styles);
+document.adoptedStyleSheets = [resetStyles];
+document.adoptedStyleSheets = [styles];
 
 const app = (() => {
     console.log("TESTING: KPINC425");
